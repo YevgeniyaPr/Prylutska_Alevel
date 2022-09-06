f9f71340a4c35c25008e34b6ff40330cf96c7186
YevgeniyaPr <112964123+YevgeniyaPr@users.noreply.github.com>
2022-09-06 21:33:55 +0300

    Add files via upload

---
+++ ProgramHwOne.java
@@ -0,0 +1,11 @@
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+public class ProgramHwOne {
+
+    public static void main(String[] args) {
+        System.out.println("Jane Prylutska");
+        LocalDateTime.now();
+        System.out.println(LocalDateTime.now());
+    }
+}
